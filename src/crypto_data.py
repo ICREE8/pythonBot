7f888466b5ea410e557447925283c587ef8c05f2
Hugo <66896648+ICREE8@users.noreply.github.com>
2024-11-01 00:27:05 -0400

    Add connectivity resources crypto_data.py

--- src/crypto_data.py
+++ src/crypto_data.py
@@ -4,3 +4,13 @@ def get_crypto_data():
     url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd'
     response = requests.get(url)
     return response.json()
+
+def get_chainlink_data():
+    url = 'CHAINLINK_COORDINATES_HERE'
+    response = requests.get(url)
+    return response.json()
+
+def get_alpaca_data():
+    url = 'ALPACA_API_RPC_URL'
+    response = requests.get(url)
+    return response.json()
